f11357149bef52b1b93a55c7734d5e5ec5354f32
MichaelElvebakken <michaelingemann@hotmail.com>
2024-08-04 23:55:59 +0200

    Graphs are now being rendered as images and put into the PDF

--- makeGraphs.py
+++ makeGraphs.py
@@ -13,47 +13,49 @@ stock = yf.Ticker("EQNR")
 def makeIncomeGraph(stck):
     income_data = stck.income_stmt
     x = stck.income_stmt.columns
+    
+    fig, ax = plt.subplots()
 
     plt.gca().xaxis.axis_date
     plt.ylabel('Billions of USD')
-    plt.plot(x, income_data.loc['Total Revenue'] / 1000000000, linestyle='solid', label='Total Revenue', color='blue')
-    plt.plot(x, income_data.loc['Normalized EBITDA'] / 1000000000, linestyle='solid', label='EBITDA', color='green')
-    plt.plot(x, income_data.loc['Operating Income'] / 1000000000, linestyle='solid', label='Operating Income', color='black')
-    plt.plot(x, income_data.loc['Net Income'] / 1000000000, linestyle='dashed', label='Net Income', color='black')
-    plt.plot(x, income_data.loc['Total Expenses'] / 1000000000, linestyle='dashed', label='Total Expenses', color='grey')
-    plt.legend(loc='best')
-    plt.title('Company Performance')
+    ax.plot(x, income_data.loc['Total Revenue'] / 1000000000, linestyle='solid', label='Total Revenue', color='blue')
+    ax.plot(x, income_data.loc['Normalized EBITDA'] / 1000000000, linestyle='solid', label='EBITDA', color='green')
+    ax.plot(x, income_data.loc['Operating Income'] / 1000000000, linestyle='solid', label='Operating Income', color='black')
+    ax.plot(x, income_data.loc['Net Income'] / 1000000000, linestyle='dashed', label='Net Income', color='black')
+    ax.plot(x, income_data.loc['Total Expenses'] / 1000000000, linestyle='dashed', label='Total Expenses', color='grey')
+    ax.legend(loc='best')
+    ax.set_title('Company Performance')
 
-    plt.show()
-    print("")
+    return fig
 
 def makeDebtGraph(stck):
     debt_data = stock.balance_sheet
     x = debt_data.columns
+    
+    fig, ax = plt.subplots()
 
     plt.gca().xaxis.axis_date
     plt.ylabel('Billions of USD')
-    plt.plot(x, debt_data.loc['Net Debt'] / 1000000000, linestyle='solid', label='Net Debt', color='blue')
-    plt.plot(x, debt_data.loc['Total Debt'] / 1000000000, linestyle='solid', label='Total Debt', color='black')
-    plt.legend(loc='upper right')
-    plt.title('Company Debt')
+    ax.plot(x, debt_data.loc['Net Debt'] / 1000000000, linestyle='solid', label='Net Debt', color='blue')
+    ax.plot(x, debt_data.loc['Total Debt'] / 1000000000, linestyle='solid', label='Total Debt', color='black')
+    ax.legend(loc='upper right')
+    ax.set_title('Company Debt')
 
-    plt.show()
-    print("")
+    return fig
 
 def makeDividendGraph(stck):
+    
     divi_data = stock.dividends
     #Resize the data to consist of 2 columns with each row representing one year
     divi_data_y = pd.DataFrame(divi_data.resample('YE').sum())
     divi_data_y.reset_index(inplace=True)
     divi_data_y['Year'] = divi_data_y['Date'].dt.year
 
+    fig, ax = plt.subplots()
+
     plt.gca().xaxis.axis_date
     plt.xlabel('Years')
     plt.ylabel('Dividend payed out (USD)')
-    plt.bar(divi_data_y['Year'], divi_data_y['Dividends'], label='Dividends (USD)')
+    ax.bar(divi_data_y['Year'], divi_data_y['Dividends'], label='Dividends (USD)')
 
-    plt.show()
-    print("")
-
-makeDividendGraph(stock)
+    return fig
--- makeReport.py
+++ makeReport.py
@@ -1,11 +1,13 @@
 import yfinance as yf
 import matplotlib.pyplot as plt
 import pandas as pd
+import io
 
 #reportlab imports - Using Platypus for simplicity 
 from reportlab.lib.pagesizes import letter
-from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph, Spacer, Frame, Flowable
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph, Spacer, Frame, Flowable, Image
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.lib.utils import ImageReader
 from reportlab.lib.units import cm
 
 import makeGraphs as mg
@@ -34,9 +36,26 @@ def main():
     elements.append(Paragraph('<strong>Industry: </strong>' + stock.info['industry'], line_spacing))
     elements.append(Paragraph('<strong>Description: </strong><br/>' + stock.info['longBusinessSummary'], line_spacing))
 
-    print(stock.info)
+    #Add graphs to PDF doc
+    income_graph_image = Image(plot_to_buffer(mg.makeIncomeGraph(stock)), 15*cm, 10*cm)
+    debt_graph_image = Image(plot_to_buffer(mg.makeDebtGraph(stock)), 15*cm, 10*cm)
+    dividend_graph_image = Image(plot_to_buffer(mg.makeDividendGraph(stock)), 15*cm, 10*cm)
+
+    elements.append(income_graph_image)
+    elements.append(debt_graph_image)
+    elements.append(dividend_graph_image)
 
     pdf.build(elements)
+
+#Rendering plots returned from makeGraphs to a Buffer, so that it can be put in the PDF / Using a buffer so the image does not have to be saved
+def plot_to_buffer(fig):
+    buffer = io.BytesIO() 
+    fig.savefig(buffer, format='jpg')
+    buffer.seek(0)
+    return buffer
+
+
+
 
 if __name__ == "__main__":
     main()
